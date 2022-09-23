07dbb0720ad5a50db8068be931d8cfc9939e3ab2
ysw1998518 <63027695+ysw1998518@users.noreply.github.com>
2022-09-23 11:43:40 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -78,8 +78,8 @@ export const USER_CONFIG = {
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        //{"type": "*生日", "name": "老婆", "year": "1999", "date": "03-08"},
-        //{"type": "节日", "name": "周年纪念日", "year": "2021", "date": "09-15"},
+        {"type": "*生日", "name": "老婆", "year": "1999", "date": "03-08"},
+        {"type": "节日", "name": "周年纪念日", "year": "2021", "date": "09-15"},
   
        
       ],
@@ -180,11 +180,11 @@ export const USER_CONFIG = {
    */
   FESTIVALS: [
     // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-    {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09", isShowAge: true},
-    {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
+   // {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09", isShowAge: true},
+  //  {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
     // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-    {"type": "生日", "name": "李四", "year": "1996", "date": "09-31", isShowAge: true},
-    {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+   // {"type": "生日", "name": "李四", "year": "1996", "date": "09-31", isShowAge: true},
+   // {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
     // 你可以不断按格式往下增加
     // ...
   ],
@@ -204,11 +204,11 @@ export const USER_CONFIG = {
    * */
   CUSTOMIZED_DATE_LIST: [
     // 在一起的日子
-    {"keyword": "love_day", date: "2022-09-08"},
+   // {"keyword": "love_day", date: "2022-09-08"},
     // 结婚纪念日
-    {"keyword": "marry_day", date: "2022-09-09"},
+   // {"keyword": "marry_day", date: "2022-09-09"},
     // 退伍日
-    {"keyword": "ex_day", date: "2022-09-10"},
+   // {"keyword": "ex_day", date: "2022-09-10"},
     // 你可以不断按格式往下增加
     // ...
   ],
